2751026ed39493018f26a0ef961b9e32aa017d83
Stuart Alldritt <s.k.alldritt@gmail.com>
2022-07-05 08:15:56 -0700

    remove build folder

--- build/zephyr/zephyr.dts.d
+++
@@ -1,6 +0,0 @@
-empty_file.o: \
- C:/Users/skall/source/repos/zephyr-3.1.0/zephyr/misc/empty_file.c \
- C:/Users/skall/source/repos/zephyr-3.1.0/zephyr/boards/arm/qemu_cortex_m3/qemu_cortex_m3.dts \
- C:/Users/skall/source/repos/zephyr-3.1.0/zephyr/dts/arm/ti/lm3s6965.dtsi \
- C:/Users/skall/source/repos/zephyr-3.1.0/zephyr/dts/arm/armv7-m.dtsi \
- C:/Users/skall/source/repos/zephyr-3.1.0/zephyr/dts/common/skeleton.dtsi
